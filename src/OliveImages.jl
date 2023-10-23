ec00fc472ad17f8ec35575d7af2951edfcf04064
emmy <52672675+emmaccode@users.noreply.github.com>
2023-10-23 08:07:33 -0600

    completed all image cells

--- src/OliveImages.jl
+++ src/OliveImages.jl
@@ -23,7 +23,7 @@ end
 
 function build(c::Connection, cm::ComponentModifier, cell::Cell{:vimage}, proj::Project{<:Any})
     newdiv = div("cellcontainer$(cell.id)")
-    style!(newdiv, "padding" => 12px, "border-radius" => 0px)
+    style!(newdiv, "padding" => 20px, "border-radius" => 0px)
     newdiv[:text] = cell.source
     newdiv::Component{:div}
 end
@@ -41,10 +41,32 @@ end
 
 function build(c::Connection, cm::ComponentModifier, cell::Cell{:image}, proj::Project{<:Any})
     newdiv = div("cellcontainer$(cell.id)")
-    style!(newdiv, "padding" => 12px, "border-radius" => 0px)
+    style!(newdiv, "padding" => 20px, "border-radius" => 0px)
     img = base64img("cell$(cell.id)", cell.outputs, cell.source)
     push!(newdiv, img)
     newdiv::Component{:div}
+end
+
+function build(c::Connection, cell::Cell{:gif}, d::Directory{<:Any})
+    base = Olive.build_base_cell(c, cell, d)
+    style!(base, "background-color" => "darkorange")
+    base::Component{:div}
+end
+
+function olive_read(cell::Cell{:gif})
+    img = img_obj = load(cell.outputs)
+    Vector{Cell{<:Any}}([Cell(1, "image", "gif", img)])::Vector{Cell{<:Any}}
+end
+
+function build(c::Connection, cell::Cell{:jpg}, d::Directory{<:Any})
+    base = Olive.build_base_cell(c, cell, d)
+    style!(base, "background-color" => "darkorange")
+    base::Component{:div}
+end
+
+function olive_read(cell::Cell{:jpg})
+    img = img_obj = load(cell.outputs)
+    Vector{Cell{<:Any}}([Cell(1, "image", "jpg", img)])::Vector{Cell{<:Any}}
 end
 
 end # module OliveImages
