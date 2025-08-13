4249b6215e40fb733b26e5b23e04d12d9f08e1b7
emmy <52672675+emmaccode@users.noreply.github.com>
2025-08-13 14:58:22 -0600

    brought into `Olive` `0.2` :)

--- src/OliveImages.jl
+++ src/OliveImages.jl
@@ -4,9 +4,8 @@ using Images
 using ImageIO
 using FileIO
 using Olive.Toolips
+using Olive.Toolips.Components
 using Olive.ToolipsSession
-using Olive.ToolipsDefaults
-using Olive.ToolipsBase64
 using Olive: getname, Project, Directory, Cell
 import Olive: build, olive_read
 
@@ -18,14 +17,7 @@ end
 
 function olive_read(cell::Cell{:svg})
     src = read(cell.outputs, String)
-    Vector{Cell{<:Any}}([Cell(1, "vimage", src)])::Vector{Cell{<:Any}}
-end
-
-function build(c::Connection, cm::ComponentModifier, cell::Cell{:vimage}, proj::Project{<:Any})
-    newdiv = div("cellcontainer$(cell.id)")
-    style!(newdiv, "padding" => 20px, "border-radius" => 0px)
-    newdiv[:text] = cell.source
-    newdiv::Component{:div}
+    Vector{Cell{<:Any}}([Cell{:vimage}(src)])::Vector{Cell{<:Any}}
 end
 
 function build(c::Connection, cell::Cell{:png}, d::Directory{<:Any})
@@ -36,16 +28,9 @@ end
 
 function olive_read(cell::Cell{:png})
     img = img_obj = load(cell.outputs)
-    Vector{Cell{<:Any}}([Cell(1, "image", "png", img)])::Vector{Cell{<:Any}}
+    Vector{Cell{<:Any}}([Cell{:image}("png", img)])::Vector{Cell{<:Any}}
 end
 
-function build(c::Connection, cm::ComponentModifier, cell::Cell{:image}, proj::Project{<:Any})
-    newdiv = div("cellcontainer$(cell.id)")
-    style!(newdiv, "padding" => 20px, "border-radius" => 0px)
-    img = base64img("cell$(cell.id)", cell.outputs, cell.source)
-    push!(newdiv, img)
-    newdiv::Component{:div}
-end
 
 function build(c::Connection, cell::Cell{:gif}, d::Directory{<:Any})
     base = Olive.build_base_cell(c, cell, d)
@@ -54,8 +39,8 @@ function build(c::Connection, cell::Cell{:gif}, d::Directory{<:Any})
 end
 
 function olive_read(cell::Cell{:gif})
-    img = img_obj = load(cell.outputs)
-    Vector{Cell{<:Any}}([Cell(1, "image", "gif", img)])::Vector{Cell{<:Any}}
+    img = load(cell.outputs)
+    Vector{Cell{<:Any}}([Cell{:image}(cell.source, img)])::Vector{Cell{<:Any}}
 end
 
 function build(c::Connection, cell::Cell{:jpg}, d::Directory{<:Any})
@@ -65,8 +50,27 @@ function build(c::Connection, cell::Cell{:jpg}, d::Directory{<:Any})
 end
 
 function olive_read(cell::Cell{:jpg})
-    img = img_obj = load(cell.outputs)
-    Vector{Cell{<:Any}}([Cell(1, "image", "jpg", img)])::Vector{Cell{<:Any}}
+    img = load(cell.outputs)
+    Vector{Cell{<:Any}}([Cell{:image}(cell.source, "jpg", img)])::Vector{Cell{<:Any}}
+end
+
+
+function build(c::Connection, cm::ComponentModifier, cell::Cell{:vimage}, proj::Project{<:Any})
+    newdiv = div("cellcontainer$(cell.id)")
+    on(c, newdiv, "dblclick") do cm::ComponentModifier
+        alert!(cm, "editing mode not yet available")
+    end
+    style!(newdiv, "padding" => 20px, "border-radius" => 0px)
+    newdiv[:text] = cell.source
+    newdiv::Component{:div}
+end
+
+function build(c::Connection, cm::ComponentModifier, cell::Cell{:image}, proj::Project{<:Any})
+    newdiv = div("cellcontainer$(cell.id)")
+    style!(newdiv, "padding" => 20px, "border-radius" => 0px)
+    img = base64img("cell$(cell.id)", cell.outputs)
+    push!(newdiv, img)
+    newdiv::Component{:div}
 end
 
 end # module OliveImages
