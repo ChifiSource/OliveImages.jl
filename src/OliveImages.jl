1b6681f5c0e373be778651d8f8ee147fd246f619
emmy <52672675+emmaccode@users.noreply.github.com>
2025-08-13 18:28:50 -0600

    added extensible images bar

--- src/OliveImages.jl
+++ src/OliveImages.jl
@@ -7,7 +7,7 @@ using Olive.Toolips
 using Olive.Toolips.Components
 using Olive.ToolipsSession
 using Olive: getname, Project, Directory, Cell
-import Olive: build, olive_read
+import Olive: build, olive_read, OliveExtension
 
 function build(c::Connection, cell::Cell{:svg}, d::Directory{<:Any})
     base = Olive.build_base_cell(c, cell, d)
@@ -69,8 +69,48 @@ function build(c::Connection, cm::ComponentModifier, cell::Cell{:image}, proj::P
     newdiv = div("cellcontainer$(cell.id)")
     style!(newdiv, "padding" => 20px, "border-radius" => 0px)
     img = base64img("cell$(cell.id)", cell.outputs)
+    on(c, newdiv, "dblclick") do cm::ComponentModifier
+        if "imgbar$(cell.id)" in cm
+            remove!(cm, "imgbar$(cell.id)")
+            return
+        end
+        newbar = build_image_bar(c, cm, cell)
+        insert!(cm, "cellcontainer$(cell.id)", 1, newbar)
+    end
     push!(newdiv, img)
     newdiv::Component{:div}
+end
+
+build_image_cell_button(oe::Type{OliveExtension{:change}}, c::AbstractConnection, cell::Cell{<:Any}) = begin
+    icon = Olive.topbar_icon("openimg$(cell.id)", "file_open")
+    style!(icon, "font-size" => 13pt, "color" => "white")
+    icon
+end
+
+
+build_vimage_cell_button(oe::Type{OliveExtension{:change}}, c::AbstractConnection, cell::Cell{<:Any}) = begin
+    a(text = "hello")
+end
+
+
+function build_image_bar(c::AbstractConnection, cm::Components.AbstractComponentModifier, cell::Cell{:vimage})
+    buttons = Vector{AbstractComponent}([begin 
+        ext = m.sig.parameters[2]
+        build_image_cell_button(ext, c, cell)
+    end for m in methods(build_vimage_cell_button)])
+    bar = div("imgbar$(cell.id)", children = buttons)
+end
+
+function build_image_bar(c::AbstractConnection, cm::Components.AbstractComponentModifier, cell::Cell{:image})
+    buttons = Vector{AbstractComponent}([begin 
+        ext = m.sig.parameters[2].parameters[1]
+        build_image_cell_button(ext, c, cell)
+    end for m in methods(build_image_cell_button)])
+
+    bar = div("imgbar$(cell.id)", children = buttons)
+    style!(bar, "padding" => .5percent, "border-bottom-left-radius" => 0px, "border-bottom-left-radius" => 0px, "background-color" => "#1e1e1e", "border-radius" => 2px, 
+    "border-bottom" => "black")
+    bar::Component{:div}
 end
 
 end # module OliveImages
