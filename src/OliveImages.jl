b76e91a91d2eeaa06ab047a7fc145fa36746baff
emmy <52672675+emmaccode@users.noreply.github.com>
2025-08-14 16:24:36 -0600

    completed display cell edit :)

--- src/OliveImages.jl
+++ src/OliveImages.jl
@@ -31,7 +31,7 @@ end
 
 function olive_read(cell::Cell{:png})
     img = load(cell.outputs)
-    Vector{Cell{<:Any}}([Cell{:image}("PNG", cell.source => img)])::Vector{Cell{<:Any}}
+    Vector{Cell{<:Any}}([Cell{:image}("PNG", [cell.source, img, "300", "left"])])::Vector{Cell{<:Any}}
 end
 
 
@@ -86,7 +86,7 @@ end
 # base64_to_image(replace(img[:src], "data:image/png;base64," => ""))
 
 function build(c::Connection, cm::ComponentModifier, cell::Cell{:image}, proj::Project{<:Any})
-    newdiv = div("cellcontainer$(cell.id)")
+    newdiv = div("cellcontainer$(cell.id)", align = cell.outputs[4])
     style!(newdiv, "padding" => 20px, "border-radius" => 0px)
     if typeof(cell.outputs) <: AbstractString
         if cell.outputs == ""
@@ -98,7 +98,8 @@ function build(c::Connection, cm::ComponentModifier, cell::Cell{:image}, proj::P
         cell.source = replace(cell.source, "# " => "")
     end
     img = base64img("cell$(cell.id)", cell.outputs[2], lowercase(cell.source))
-    on(c, newdiv, "dblclick") do cm::ComponentModifier
+    img[:width] = cell.outputs[3]
+    on(c, img, "dblclick") do cm::ComponentModifier
         if "imgbar$(cell.id)" in cm
             remove!(cm, "imgbar$(cell.id)")
             return
@@ -120,11 +121,42 @@ function build_fileseeker()
 
 end
 
+function image_detail_edit(c::AbstractConnection, cell::Cell{:image}, proj::Olive.Project)
+    cellid = cell.id
+    backbutton = Olive.topbar_icon("bac$cellid", "arrow_back")
+    namebox = textdiv("namebox$cellid", text = cell.outputs[1])
+    style!(backbutton, "font-size" => 16pt, "margin-right" => 10px, "color" => "white")
+    style!(namebox, "border-radius" => 3px, "background-color" => "white", "display" => "inline-block")
+    alignbox_options = Components.options("left", "center", "right")
+    alselector = Components.select("alsel$cellid", alignbox_options, value = cell.outputs[4])
+    xbox = Components.numberinput("xinp$cellid", range = 1:5000, value = cell.outputs[3])
+    widthlabel = a(text = "width: ")
+    alignlabel = a(text = "alignment: ")
+    nimps = div("-", children = [alignlabel, alselector, widthlabel, xbox])
+    style!(nimps, "display" => "inline-block")
+    confbutton = Olive.topbar_icon("conf$cellid", "check_circle")
+    on(c, confbutton, "click") do cm::ComponentModifier
+        newx = cm["xinp$cellid"]["value"]
+        new_alignment = cm["alsel$cellid"]["value"]
+        newname = cm["namebox$cellid"]["text"]
+        cm["cell$cellid"] = "width" => newx
+        cm["cellcontainer$cellid"] = "align" => new_alignment
+        cell.outputs[1] = newname
+        cell.outputs[3] = newx
+        cell.outputs[4] = new_alignment
+        newbar = build_image_bar(c, cm, cell, proj)
+        remove!(cm, newbar)
+        insert!(cm, "cellcontainer$(cell.id)", 1, newbar)
+    end
+    style!(confbutton, "font-size" => 16pt, "margin-left" => 10px, "color" => "white")
+    div("imgedit$cellid", children = [backbutton, namebox, nimps, confbutton])
+end
+
 build_image_cell_button(oe::Type{OliveExtension{:change}}, c::AbstractConnection, cell::Cell{<:Any}, proj::Olive.Project) = begin
     cellid::String = cell.id
     icon = buildbase_imgcell_button("openimg", cellid, "file_open")
     on(c, icon, "click") do cm::ComponentModifier
-        set_children!(cm, "imgbar$cellid", )
+        
     end
     icon
 end
@@ -132,7 +164,8 @@ end
 build_image_cell_button(oe::Type{OliveExtension{:resize}}, c::AbstractConnection, cell::Cell{<:Any}, proj::Olive.Project) = begin
     icon = buildbase_imgcell_button("resimg", cell.id, "settings_overscan")
     on(c, icon, "click") do cm::ComponentModifier
-
+        detail_editor = image_detail_edit(c, cell, proj)
+        set_children!(cm, "imgbar$(cell.id)", [detail_editor])
     end
     style!(icon, "font-size" => 16pt, "color" => "white")
     icon
@@ -188,12 +221,15 @@ function build_image_bar(c::AbstractConnection, cm::Components.AbstractComponent
     bar::Component{:div}
 end
 
-string(cell::Cell{:image}) = begin
-
+function string(cell::Cell{:image})
+    celltype::String = string(typeof(cell).parameters[1])
+    fname = cell.outputs[1]
+    image = image_to_base64(cell.outputs[2])
+    return(*("# $(cell.source)", "\n#==output[$celltype]\n$(fname)!|$image\n==#\n#==|||==#\n"))::String
 end
 
 string(cell::Cell{:vimage}) = begin
-
+    return(*("# svg image:", "\n#==output[$celltype]\n$(cell.source)\n==#\n#==|||==#\n"))::String
 end
 
 end # module OliveImages
